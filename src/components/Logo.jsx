ca4514991ccd6bf9cc6c032ed7d47fd4359b8f6c
idealoshi <idealoshi11@gmail.com>
2024-06-14 19:32:43 +0200

    Updates

--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import logo from "../images/newlogo.png";
-import logo from "../images/logo500x420.png";
-import styles from "./Logo.module.css";
-
-export default function Logo() {
-  return <img src={logo} alt="IdealLogo" className={styles.logo} />;
-}
